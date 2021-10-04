bce9ddb906a0899d289def6de6903cc2f833cb2f
Jmucheke <jmucheke274@gmail.com>
2021-10-04 14:38:19 -0700

    Function that prints according to a given format

--- printf.c
+++ printf.c
@@ -1,7 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
-#include <stdarg.h>
 
 /**
  * check_for_specifiers - check for a valid format specifier
@@ -15,17 +13,6 @@ static int (*check_for_specifiers(const char *format))(va_list)
 	print_t p[] = {
 		{"c", print_c},
 		{"s", print_s},
-		{"i", print_i},
-		{"d", print_d},
-		{"u", print_u},
-		{"b", print_b},
-		{"o", print_o},
-		{"x", print_x},
-		{"X", print_X},
-		{"p", print_p},
-		{"S", print_S},
-		{"r", print_r},
-		{"R", print_R},
 		{NULL, NULL}
 	};
 
