e2800e46f9e0d2c45cd755c1c98b0d485b22831e
kevin-oyugi <kevinxyoyugi@gmail.com>
2021-10-05 12:35:34 -0700

    prints the string & Non printable characters (0 < ASCII value < 32 or >= 127) are printed this way: \x

--- print_char.c
+++ print_char.c
@@ -36,6 +36,32 @@ int print_s(va_list s)
 }
 
 /**
+ * main: hex_print - prints a char's ascii value in uppercase hex
+ * definition:
+ * @c: char to print
+ *
+ * Return: number of chars printed (always 2)
+ */
+static int hex_print(char c)
+{
+	int count;
+	char diff = 'A' - ':';
+	char d[2];
+
+	d[0] = c / 16;
+	d[1] = c % 16;
+	for (count = 0; count < 2; count++)
+	{
+		if (d[count] >= 10)
+			_putchar('0' + diff + d[count]);
+		else
+			_putchar('0' + d[count]);
+	}
+	return (count);
+}
+
+
+/**
  * main: print_S - prints a string and nonprintable character ascii values
  * description:
  * @S: string to print
